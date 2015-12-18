a59018f00b4ba21a620b0868c81ce2c840036fa9
Gulmohar <gulmohar96@gmail.com>
2015-12-18 21:32:21 +1100

    First commit,random file

---
+++ firstcommit.py
@@ -0,0 +1,6 @@
+#FIRST COMMIT
+# So yea guys, this is a chill thing, we make robots, we dont keep good a goodreadmefile but this is us.
+# alrighty then 
+# xoxo
+
+print('We mean only nerdiness. We also nerd better than you.(at least we claim to)')
